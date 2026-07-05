31f0e99a24a57f92d8601cea6bd8780b2dd4ee70
DewenLei04 <leidewen2004@gmail.com>
2026-07-04 23:10:08 -0400

    style fix

---
+++ src/data/siteLinks.ts
@@ -0,0 +1,6 @@
+export const siteLinks = {
+  email: 'mailto:dewen@example.com',
+  github: 'https://github.com/DewenLei04',
+  linkedin: 'https://www.linkedin.com/in/dewenlei/',
+  resume: '/resume.pdf',
+};
